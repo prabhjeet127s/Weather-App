4b6458fcfb4217bc8883c36f619446e8adef9e30
prabhjet singh <prabhjeet127.s@gmail.com>
2025-07-23 17:44:51 +0530

    basic ui setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import SearchBar from "./components/SearchBar"
+
+
+function App() {
+  
+  return (
+    <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center "> 
+    
+        <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md">
+       <h1 className="text-3xl font-bold text-center mb-6 ">Weather App</h1>
+       <SearchBar/>
+
+
+
+
+            </div>
+
+
+
+    
+    </div>
+  )
+}
+
+export default App

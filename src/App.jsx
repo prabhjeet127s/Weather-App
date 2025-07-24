ca9440e5e3c03f52bb89ae2da0a5c9053cd42897
prabhjet singh <prabhjeet127.s@gmail.com>
2025-07-24 16:25:18 +0530

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
-import SearchBar from "./components/SearchBar"
 
+import { FaSearch } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
 
 function App() {
+ 
   
   return (
-    <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center "> 
-    
-        <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md">
-       <h1 className="text-3xl font-bold text-center mb-6 ">Weather App</h1>
-       <SearchBar/>
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-orange-500 to-black text-white">
+        <h1 className="my-12 text-gray-200 font-semibold">Weather App</h1>
+        {/* search bar & icon */}
+        <div className="flex items-center bg-white rounded-full px-4 py-2 mb-6 w-80 shadow-lg">
+          <input
+            type="text"
+            placeholder="Search"
+            
+            className="flex-1 text-black outline-none px-2"
+          />
+          <FaSearch
+           
+            className="text-gray-500 cursor-pointer"
+          />
+        </div>
 
+        {/* weather icon */}
+        <img
+          src={``}
+          alt=""
+          className="w-20 h-20 mb-4"
+        />
 
+        {/* Temprature & CityName */}
+        <h1 className="text-4xl font-bold mb-1">
+        </h1>
+        <h2 className="text-2xl mt-2 font-semibold">
+        </h2>
 
-
-            </div>
-
-
-
-    
-    </div>
-  )
+        {/* Humadity & Wind Speed */}
+        <div className="w-full max-w-md mt-7 flex flex-col md:flex-row items-center justify-between md:items-start">
+          <div className="flex flex-col items-center ">
+            <WiHumidity className="text-3xl" />
+            <span className="text-lg font-medium">
+              80%
+            </span>
+            <p className="text-sm">Humidity</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <WiStrongWind className="text-3xl" />
+            <span className="text-lg font-medium">
+            100 km/h
+            </span>
+            <p className="text-sm">Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+
+export default App;
